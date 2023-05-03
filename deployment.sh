f241b25dc437e6b33048c5ede6ba4c2c9dad74d3
Asad Iqbal <asad.iqbal@fiveriverstech.com>
2023-05-03 16:16:15 +0500

    changes in jenkins file

--- deployment.sh
+++ deployment.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 pwd
-# sudo mkdir app2
 cd /home/ubuntu//home/ubuntu/react-demo-app-1
 git pull
 npm install
-# git checkout $1
-# yarn install
-# yarn build
-# pm2 restart frontend
 exit
